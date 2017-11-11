14d9ec6e4bd3b24383e5c93453aa24afeb52b649
Victor Jiao <VjiaoBlack@gmail.com>
2017-11-10 18:20:10 -0600

    added port

--- server.js
+++ server.js
@@ -230,6 +230,6 @@ app.use("*", function(req, res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Live at Port 3000");
 });
